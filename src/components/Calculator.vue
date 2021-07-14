2dd309bc4bfb52004b56a0bc50a2e0718ab36bec
FakeStandard <rileylin.space@gmail.com>
2021-07-14 14:49:55 +0800

    sort out

---
+++ src/components/Calculator.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="calculator">
+    <!--Header-->
+    <h1 align="center">
+      <span style="font-family: fantasy">2021 </span>
+      <span style="font-family: Microsoft JhengHei">勞健保試算</span>
+    </h1>
+    <!--Table-->
+    <el-table :data="info" style="80%" border stripe>
+      <!-- <el-table-column prop="a" label="級距"></el-table-column> -->
+    </el-table>
+  </div>
+</template>
